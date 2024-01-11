ede460e47304833b48c25e036604fc3d24b148ce
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-01-11 13:32:04 -0500

    Time: 3 ms (85.74%) | Memory: 8.1 MB (87.99%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    int helper(int n )
-    {
-        int count = 0;
-        while(n>0)
-        {
-            count += 1;
-            n = n & (n-1);
-        }
-        return count;
-    }
     vector<int> countBits(int n) {
         vector<int>res(n+1);
-        for(int i = 0; i<=n;i++)
+        res[0] = 0;
+        for(int i = 1; i<=n;i++)
         {
-           res[i] = helper(i);
+           res[i] = res[i/2] + i % 2;
         }
         return res;
     }
