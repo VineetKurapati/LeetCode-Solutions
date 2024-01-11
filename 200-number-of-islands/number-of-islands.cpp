40f34f05f385c94ff6be9906845bdd417164a651
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-01-11 11:08:20 -0500

    Time: 21 ms (97.50%) | Memory: 12.5 MB (86.63%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
+    int count = 0;
     void helper(int i, int j, vector<vector<char>>& grid, int n, int m)
     {
-        if(i < 0 || j < 0 || i == n || j == m || grid[i][j] != '1' ) return;
-        grid[i][j] = 2;
-        helper(i+1, j, grid, n,m);
-        helper(i, j+1, grid, n, m);
+        if(i < 0 || j<0 || i == n || j == m || grid[i][j]!='1') return;
+        grid[i][j] = '2';
+        helper(i+1, j, grid, n, m);
         helper(i-1, j , grid, n, m);
         helper(i, j-1, grid, n, m);
-
+        helper(i, j+1, grid, n, m);
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int count = 0;
         for(int i = 0; i<n ;i++)
         {
-            for(int j = 0; j<m; j++)
+            for(int j = 0;j<m;j++)
             {
                 if(grid[i][j] == '1')
                 {
-                    helper(i, j , grid, n, m);
+                    helper(i, j, grid, n, m);
                     count+=1;
                 }
             }
