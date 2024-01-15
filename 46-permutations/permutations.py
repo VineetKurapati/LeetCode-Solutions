d38a4013827dbce9cb2775489d11f7947c6c2409
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-01-15 12:08:34 -0500

    Time: 63 ms (9.74%) | Memory: 17.4 MB (30.92%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,7 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        st = set()
+        while(len(st) != factorial(len(nums))):
+            shuffle(nums)
+            st.add(tuple(nums))
+        return st
