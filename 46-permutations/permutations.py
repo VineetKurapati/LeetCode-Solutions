dcb861a72cdd7d4db216241f74f012feb2e73d90
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-01-15 12:10:08 -0500

    Time: 36 ms (92.79%) | Memory: 17.5 MB (30.92%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,7 +1,17 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        st = set()
-        while(len(st) != factorial(len(nums))):
-            shuffle(nums)
-            st.add(tuple(nums))
-        return st
+        def backtrack(nums, curr):
+            if len(curr) == len(nums):
+                result.append(curr[:])
+                return
+            
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    backtrack(nums, curr)
+                    curr.pop()
+            
+
+        result = []
+        backtrack(nums, [])
+        return result
