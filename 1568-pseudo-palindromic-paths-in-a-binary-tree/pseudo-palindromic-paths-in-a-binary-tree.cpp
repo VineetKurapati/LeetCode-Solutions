28dc78136fe17c7c8b0a40ed344100a240d119d5
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-01-24 11:12:36 -0500

    Time: 254 ms (80.16%) | Memory: 190.4 MB (41.02%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,42 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 private:
-    bool isPseudoPalindromic(const unordered_map<int, int>& freq) {
+    bool isPalindromic(const unordered_map<int,int>& freq)
+    {
         int oddCount = 0;
-        for (const auto& it : freq) {
-            if (it.second % 2 == 1) {
+        for(const auto& it : freq)
+        {
+            if(it.second % 2  == 1)
+            {
                 oddCount++;
-                if (oddCount > 1) {
-                    return false;
-                }
             }
+            if(oddCount > 1) return false;
         }
         return true;
     }
-    void dfs(TreeNode* node, unordered_map<int, int>& freq, int& fnl) {
-        if (node == nullptr) {
-            return;
-        }
-        freq[node->val]++;
-        if (node->left == nullptr && node->right == nullptr) {
-            if (isPseudoPalindromic(freq)) {
+    void dfs(TreeNode* root, unordered_map<int,int>& freq, int& fnl)
+    {
+        if(root == NULL) return;
+        freq[root->val]++;
+        if(root->left == NULL && root->right == NULL)
+        {
+            if(isPalindromic(freq))
+            {
                 fnl++;
             }
         }
-        dfs(node->left, freq, fnl);
-        dfs(node->right, freq, fnl);
-        freq[node->val]--;  
-        if (freq[node->val] == 0) {
-            freq.erase(node->val);
+        dfs(root->left, freq, fnl);
+        dfs(root->right, freq, fnl);
+        freq[root->val]--;
+        if(freq[root->val] == 0)
+        {
+            freq.erase(root->val);
         }
     }
-
 public:
-    int pseudoPalindromicPaths(TreeNode* root) {
-        ios::sync_with_stdio(false); 
-        cin.tie(0); cout.tie(0);
+    int pseudoPalindromicPaths (TreeNode* root) {
         int fnl = 0;
-        unordered_map<int, int> freq;
-        dfs(root, freq, fnl);
+        unordered_map<int,int>m;
+        dfs(root, m, fnl);
         return fnl;
     }
 };
