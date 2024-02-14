e45678e91f51a2614c4902fbcaa55aa480b10680
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-02-14 11:25:04 -0600

    Time: 122 ms (94.23%) | Memory: 18.3 MB (60.25%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -1,12 +1,9 @@
 class Solution:
     def removeStars(self, s: str) -> str:
         res = []
-        for char in s:
-            if char != '*':
-                res.append(char)
+        for ch in s:
+            if ch != "*":
+                res.append(ch)
             else:
                 res.pop()
-        ans = ""
-        for char in res:
-            ans+=char
-        return ans
+        return "".join(res)
