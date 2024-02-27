54b9da6e5ee187bfc45edaa31172b1ad18e928e5
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-02-27 11:33:40 -0600

    Time: 65 ms (45.53%) | Memory: 17.6 MB (88.70%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,13 +1,8 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp1 = [triangle[0][0]]
-        for d in range(1, len(triangle)):
-            dp1 = [
-                min(
-                    dp1[i] if i < len(dp1) else float("Inf"),
-                    dp1[i - 1] if i > 0 else float("Inf"),
-                )
-                + v
-                for i, v in enumerate(triangle[d])
-            ]
-        return min(dp1)
+        for i in range(len(triangle) - 2, -1, -1):
+        # Update each element in the current row with the minimum path sum to reach it
+            for j in range(len(triangle[i])):
+                    triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+        # The minimum path sum to reach the top node will be stored in triangle[0][0]
+        return triangle[0][0]
