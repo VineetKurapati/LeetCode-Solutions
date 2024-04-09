f99d080434ab8bfe97226806ffe530ff096841d2
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-04-09 15:36:57 -0500

    Time: 2 ms (57.25%) | Memory: 9.3 MB (67.22%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int n = tickets.size();
-        int count = 0;
-        while(tickets[k] != 0)
-        {
-            for(int i = 0;i<n;i++)
-            {
-                if(tickets[i] >= 1)
-                {
-                    tickets[i]-=1;
-                    count +=1;
-                }
-                if(tickets[k] == 0)
-                {
-                    break;
+        int ans = 0;
+        while(tickets[k] != 0){
+            for(auto &i : tickets){
+                if(tickets[k] == 0) break;
+                if(i != 0){
+                    i--;
+                    ans++;
                 }
             }
         }
-        return count;
+        return ans;
     }
 };
