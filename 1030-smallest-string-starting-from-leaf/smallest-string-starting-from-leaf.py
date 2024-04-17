bfd4255a2c4c6ac582652e807a0de16897d688cd
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-04-17 12:26:15 -0500

    Time: 49 ms (24.38%) | Memory: 17.7 MB (53.12%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.py
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.py
@@ -1,20 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def smallestFromLeaf(self, root: TreeNode) -> str:
-        self.ans = None
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+        self.ans = None 
         self.dfs(root, [])
         return self.ans
-
     def dfs(self, root, path):
         if not root:
-            return
-
+            return 
         path.append(chr(root.val + ord('a')))
-
-        if not root.left and not root.right:
+        if root.left is None and root.right is None:
             rev_path = ''.join(path[::-1])
-            if self.ans is None or rev_path < self.ans:
+            if self.ans is None or self.ans > rev_path:
                 self.ans = rev_path
-
         self.dfs(root.left, path)
         self.dfs(root.right, path)
         path.pop()
