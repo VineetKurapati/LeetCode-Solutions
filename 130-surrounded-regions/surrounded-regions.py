a7e3681d26202f835bfdeaa59f3e37bf35a57241
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-04-19 08:30:16 -0500

    Time: 122 ms (52.50%) | Memory: 17.8 MB (98.21%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,24 +1,19 @@
 class Solution:
     def search(self, board, i, j, n, m):
-        if i < 0 or j < 0 or i >= n or j >= m or board[i][j] != "O":
+        if i<0 or j<0 or i >= n or j >= m or board[i][j] != "O":
             return
         board[i][j] = "Y"
         self.search(board, i+1, j, n, m)
         self.search(board, i-1, j, n, m)
         self.search(board, i, j+1, n, m)
         self.search(board, i, j-1, n, m)
-
     def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
         n = len(board)
         m = len(board[0])
         for i in range(n):
             for j in range(m):
-                if (i == 0 or j == 0 or i == n - 1 or j == m - 1) and board[i][j] == "O":
+                if (i == 0 or j == 0 or j == m-1 or i == n-1) and board[i][j] == "O":
                     self.search(board, i, j, n, m)
-
         for i in range(n):
             for j in range(m):
                 if board[i][j] == "O":
