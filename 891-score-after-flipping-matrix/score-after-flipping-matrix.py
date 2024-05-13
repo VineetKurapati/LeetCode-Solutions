42835a5a2a68263583ec133c7145f06cbc561f34
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-05-12 22:37:13 -0500

    Time: 41 ms (56.47%) | Memory: 16.6 MB (18.53%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.py
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.py
@@ -1,25 +1,46 @@
-from typing import List
-
 class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
-        n, m = len(grid), len(grid[0])
-
-        # Toggle rows if the first element is 0
+        n = len(grid)
+        m = len(grid[0])
+        # Optimize Row
         for i in range(n):
             if grid[i][0] == 0:
-                grid[i] = [1 - x for x in grid[i]]  # Invert the row
-
-        # Toggle columns if the number of zeros is greater than the number of ones
-        for j in range(1, m):
-            num_ones = sum(grid[i][j] for i in range(n))
+                # Since the mostt significant bit is 0 we start flipping 
+                j = 0 
+                while j < m:
+                    if grid[i][j] == 1:
+                        grid[i][j] = 0
+                        j+=1
+                        continue
+                    else:
+                        grid[i][j] = 1
+                        j+=1
+                        continue
+        res = 0
+        # Optimizing Col 
+        for j in range(m):
+            num_zeros = 0
+            num_ones = 0
+            i = 0
+            while i < n:
+                if grid[i][j] == 1:
+                    num_ones += 1
+                i+=1
             if num_ones < n / 2:
-                for i in range(n):
-                    grid[i][j] = 1 - grid[i][j]  # Invert the column
-
-        # Calculate the final sum
+                # Flipping the col
+                i = 0
+                while i < n:
+                    if grid[i][j] == 0:
+                        grid[i][j] = 1
+                        i+=1
+                        continue
+                    else:
+                        grid[i][j] = 0
+                        i+=1
+                        continue
         res = 0
         for row in grid:
             binary_str = ''.join(map(str, row))
             res += int(binary_str, 2)
-
         return res
+                    
