75d11761b4920b382153903d5beaf1d509ad68d6
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-05-17 17:41:34 -0500

    Time: 53 ms (11.51%) | Memory: 17.1 MB (80.43%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -6,26 +6,12 @@
 #         self.right = right
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
-        def dfs(root, target):
-            if root is None:
-                return
-            if root.left is not None and root.left.val == target and root.left.left is None and root.left.right is None:
-                root.left = None
-            if root.right is not None and root.right.val == target and root.right.left is None and root.right.right is None:
-                root.right = None
-            dfs(root.left, target)
-            dfs(root.right, target)
-        def check(root, target):
-            # Function to check if target is leaf node
-            if root is None:
-                return False
-            if root.val == target and root.left is None and root.right is None:
-                return True
-            return check(root.left, target) or check(root.right, target)
-        c = True
-        while c:
-            dfs(root, target)
-            c = check(root, target)
-            if root.left is None and root.right is None and root.val == target:
-                return None
+        if not root:
+            return root
+        if root.left:
+            root.left = self.removeLeafNodes(root.left, target)
+        if root.right:
+            root.right = self.removeLeafNodes(root.right, target)
+        if not root.left and not root.right and root.val == target:
+            return None
         return root
