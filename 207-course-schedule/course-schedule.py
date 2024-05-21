0f8ee7e6b2fe1a5d32bdfcfad7680bda2b641d93
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-05-21 09:33:01 -0500

    Time: 94 ms (33.08%) | Memory: 18.4 MB (53.45%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,26 +1,22 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        vis = set()
-        graph = defaultdict(list)
-        for pre in prerequisites:
-            ai = pre[0]
-            bi = pre[1]
-            if ai not in graph:
-                graph[ai] = []
-            graph[ai].append(bi)
-        def dfs(node):
-            if node in vis:
-                return False
-            if graph[node] == []:
-                return True
-            vis.add(node)
-            for n in graph[node]:
-                if not dfs(n):
-                    return False
-            vis.remove(node)
-            graph[node] = []
-            return True
-        for node in range(numCourses):
-            if not dfs(node):
-                return False
-        return True
+    def canFinish(self, n: int, prerequisites: List[List[int]]) -> bool:
+        adj = [[] for _ in range(n)]
+        indegree = [0] * n
+        res = []
+        for p in prerequisites:
+            course = p[0]
+            pre = p[1]
+            adj[pre].append(course)
+            indegree[course] += 1
+        queue = deque()
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                queue.append(i)
+        while queue:
+            curr = queue.popleft()
+            res.append(curr)
+            for neigh in adj[curr]:
+                indegree[neigh] -=1 
+                if indegree[neigh] == 0:
+                    queue.append(neigh)
+        return len(res) == n
