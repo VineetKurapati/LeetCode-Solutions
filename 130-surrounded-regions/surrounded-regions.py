44172ca6e5f0f1ed48f2a80a6d4fd2585eb5f9e2
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-05-23 16:11:57 -0500

    Time: 110 ms (93.92%) | Memory: 17.6 MB (99.96%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,22 +1,26 @@
 class Solution:
-    def search(self, board, i, j, n, m):
-        if i<0 or j<0 or i >= n or j >= m or board[i][j] != "O":
-            return
-        board[i][j] = "Y"
-        self.search(board, i+1, j, n, m)
-        self.search(board, i-1, j, n, m)
-        self.search(board, i, j+1, n, m)
-        self.search(board, i, j-1, n, m)
     def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
         n = len(board)
         m = len(board[0])
+        def dfs(board, i, j):
+            if i < 0 or j < 0 or i>=len(board) or j >= len(board[0]) or board[i][j] != "O":
+                return 
+            board[i][j] = "S"
+            dfs(board, i+1, j)
+            dfs(board, i-1, j)
+            dfs(board, i, j+1)
+            dfs(board, i, j-1)
         for i in range(n):
             for j in range(m):
-                if (i == 0 or j == 0 or j == m-1 or i == n-1) and board[i][j] == "O":
-                    self.search(board, i, j, n, m)
+                # Checking if they are in the boundry 
+                if (i == 0 or j == 0 or i == n-1 or j == m-1) and board[i][j] == "O":
+                    dfs(board, i, j)
         for i in range(n):
             for j in range(m):
                 if board[i][j] == "O":
                     board[i][j] = "X"
-                if board[i][j] == "Y":
+                if board[i][j] == "S":
                     board[i][j] = "O"
