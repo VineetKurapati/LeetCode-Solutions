b4555faec81b4a095daedafb49715bd32f78e81e
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-05-22 22:39:22 -0500

    Time: 173 ms (46.99%) | Memory: 18 MB (78.36%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -5,12 +5,6 @@ class Solution:
             if num not in d:
                 d[num] = 0
             d[num] += 1
-        res_ele = 0
-        res_val = 0
-        for num in d:
-            if res_val < d[num]:
-                res_ele = num
-                res_val = d[num]
-        return res_ele
-        
+        res = max(d, key = d.get)
+        return res
         
