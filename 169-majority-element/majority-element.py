9b003a600d7be08ddeaa6dc65f0b944746ae93b1
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-05-22 22:36:37 -0500

    Time: 167 ms (73.65%) | Memory: 17.9 MB (78.36%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dict = {}
+        d = {}
         for num in nums:
-            if num not in dict:
-                dict[num] = 1
-            else:
-                dict[num] += 1
-        n = len(nums)
-        for key in dict:
-            value = dict[key]
-            if value > n//2:
-                return key
-        return -1
+            if num not in d:
+                d[num] = 0
+            d[num] += 1
+        res_ele = 0
+        res_val = 0
+        for num in d:
+            if res_val < d[num]:
+                res_ele = num
+                res_val = d[num]
+        return res_ele
+        
+        
