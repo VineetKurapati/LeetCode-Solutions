8dbdba3d5f50394b3db13256477f40b3d9b3c81b
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-05-26 11:09:27 -0500

    Time: 19 ms (99.81%) | Memory: 16.5 MB (87.03%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -1,18 +1,15 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         word_set = set(wordDict)
-        return self.wordBreakHelper(s, 0, word_set)
-    
-    def wordBreakHelper(self, s: str, start: int, word_set: set) -> List[str]:
-        valid_substr = []
-        
+        return self.helper(s, 0, word_set)
+    def helper(self, s, start, wordDict):
+        valid_words = []
         if start == len(s):
-            valid_substr.append("")
+            valid_words.append("")
         for end in range(start + 1, len(s) + 1):
-            prefix = s[start:end]
-            if prefix in word_set:
-                suffixes = self.wordBreakHelper(s, end, word_set)
+            prefix = s[start: end]
+            if prefix in wordDict:
+                suffixes = self.helper(s, end, wordDict)
                 for suffix in suffixes:
-                    valid_substr.append(prefix + ("" if suffix == "" else " ") + suffix)
-        
-        return valid_substr
+                    valid_words.append(prefix + ("" if suffix == "" else " ") + suffix)
+        return valid_words
