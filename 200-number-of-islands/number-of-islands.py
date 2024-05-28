ecada2a979f1da672e1941b185360fbbd8386025
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-05-28 12:15:24 -0500

    Time: 244 ms (51.21%) | Memory: 18.9 MB (80.66%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,19 +1,20 @@
 class Solution:
-    def dfs(self, grid, i, j, n, m):
-        if i < 0 or j < 0 or i >=n or j>=m or grid[i][j] != "1":
-            return 
-        grid[i][j] = "2"
-        self.dfs(grid, i+1, j, n,m)
-        self.dfs(grid, i-1, j, n,m)
-        self.dfs(grid, i, j+1, n,m)
-        self.dfs(grid, i, j-1, n,m)
     def numIslands(self, grid: List[List[str]]) -> int:
         n = len(grid)
         m = len(grid[0])
         count = 0
+        def dfs(grid, i, j, n, m):
+            if i < 0 or j < 0 or i >=n or j >= m or grid[i][j] != "1":
+                return 
+            # Marking the spot as visited
+            grid[i][j] = "2"
+            directions  = [[0,1], [0, -1], [1,0], [-1, 0]]
+            for u,v in directions:
+                dfs(grid, i+v, j+u, n, m)
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == "1":
-                    self.dfs(grid, i, j, n, m)
-                    count +=1
+                    dfs(grid, i, j, n, m)
+                    count += 1
+        
         return count
