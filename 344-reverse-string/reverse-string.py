c5df9a24bdb5ab86dbc5d5ff7b559ecb0d1c0260
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-06-01 22:28:43 -0500

    Time: 165 ms (76.74%) | Memory: 21.1 MB (14.68%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -1,8 +1,8 @@
+from typing import List
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        n = len(s)
-        for i in range(n//2):
-            s[i], s[-i-1] = s[-i-1], s[i]
+        s[:] = s[::-1]
