d3c02d585415e4b8cf741ea4c55c572106e9e981
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-06-08 12:06:18 -0500

    Time: 840 ms (33.20%) | Memory: 36.3 MB (65.28%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,15 +1,12 @@
 class Solution:
     def checkSubarraySum(self, nums, k):
-        prefix_mod = 0
-        mod_seen = {0: -1}
-
-        for i in range(len(nums)):
-            prefix_mod = (prefix_mod + nums[i]) % k
-
-            if prefix_mod in mod_seen:
-                if i - mod_seen[prefix_mod] > 1:
+        count = 0 
+        prefix_dict = {0: -1}
+        for i, num in enumerate(nums):
+            count = (count + num) % k 
+            if count in prefix_dict:
+                if i - prefix_dict[count] > 1:
                     return True
             else:
-                mod_seen[prefix_mod] = i
-
+                prefix_dict[count] = i
         return False
