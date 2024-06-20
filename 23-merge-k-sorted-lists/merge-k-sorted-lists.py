74d77551d62742fe6cb994e1ac72242d99a37e93
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-06-20 07:37:44 -0500

    Time: 73 ms (78.96%) | Memory: 19.9 MB (29.72%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -4,32 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def merge(self, list1, list2):
-        # Logic to merge two linked lists
-        curr = dummy = ListNode()
-        while list1 and list2:
-            if list2.val > list1.val:
-                curr.next = list1
-                list1 = list1.next
-            else:
-                curr.next = list2
-                list2 = list2.next
-            curr = curr.next 
-        # Append the remaining nodes from list1 or list2
-        if list1:
-            curr.next = list1
-        if list2:
-            curr.next = list2
-        return dummy.next
-
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        k = len(lists)
-        if k == 0:
-            return None 
-        if k == 1:
-            return lists[0]
-        first_list = lists[0]
-        for i in range(1, k):
-            first_list = self.merge(first_list, lists[i])
-        return first_list
-
+        curr = ListNode(0) 
+        prev = curr
+        for l in lists:
+            while l:
+                t = l.val 
+                curr.next = ListNode(t)
+                curr = curr.next
+                l = l.next
+        prev = prev.next
+        pq = []
+        res = ListNode(0)
+        ans = res
+        while prev:
+            heapq.heappush(pq, prev.val)
+            prev = prev.next
+        while pq:
+            v = heapq.heappop(pq)
+            res.next = ListNode(v)
+            res = res.next
+        return ans.next
