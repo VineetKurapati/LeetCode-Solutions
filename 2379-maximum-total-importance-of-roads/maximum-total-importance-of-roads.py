1cc5ec69ce919ee1eff08aad020800af486284c2
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-06-28 09:33:15 -0500

    Time: 1281 ms (40.16%) | Memory: 45 MB (15.16%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
@@ -1,30 +1,21 @@
-from collections import defaultdict
-import heapq
-
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
         indegree = defaultdict(int)
-        
-        # Calculate indegree for each node
         for u, v in roads:
+            if u not in indegree:
+                indegree[u] = 0
+            if v not in indegree:
+                indegree[v] = 0
             indegree[u] += 1
-            indegree[v] += 1
-        
-        # Use a max heap to assign importance efficiently
-        max_heap = [(-deg, node) for node, deg in indegree.items()]
-        heapq.heapify(max_heap)
-        
+            indegree[v] += 1 
+        indegree = sorted(indegree.items(), key=lambda x: x[1], reverse = True)
         importance = {}
-        current_importance = n
-        
-        while max_heap:
-            _, node = heapq.heappop(max_heap)
-            importance[node] = current_importance
-            current_importance -= 1
-        
-        # Calculate the total importance
+        i = 0
+        while i < len(indegree):
+            importance[indegree[i][0]] = n
+            n-=1
+            i+=1
         res = 0
-        for u, v in roads:
-            res += importance[u] + importance[v]
-        
+        for u, y in roads:
+            res += importance[u] + importance[y]
         return res
