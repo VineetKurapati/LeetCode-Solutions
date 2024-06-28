7c7d94bb5f807b4eb81da867d1380036d5830de5
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-06-28 09:15:58 -0500

    Time: 1197 ms (82.79%) | Memory: 41.5 MB (87.70%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
@@ -1,23 +1,11 @@
 class Solution:
-    def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
-        indegree = {}
-        for u, v in roads:
-            if u not in indegree:
-                indegree[u] = 0
-            if v not in indegree:
-                indegree[v] = 0
-            indegree[u] += 1
-            indegree[v] += 1 
-        indegree = sorted(indegree.items(), key=lambda x: x[1], reverse = True)
-        importance = {}
-        print(indegree)
-        i = 0
-        while i < len(indegree):
-            importance[indegree[i][0]] = n
-            n-=1
-            i+=1
-        res = 0
-        for u, y in roads:
-            res += importance[u] + importance[y]
-        return res
-        return 0
+    def maximumImportance(self, n: int, a: List[List[int]]) -> int:
+        d=[0 for i in range(n)]
+        for x,y in a:
+            d[x]+=1
+            d[y]+=1
+        d.sort()
+        ans=0
+        for i in range(1,n+1):
+            ans+=i*d[i-1]
+        return ans
