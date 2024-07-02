9176828ccc134e8ae5bde3358f5661503eacaa4a
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-07-02 09:40:43 -0500

    Time: 51 ms (54.11%) | Memory: 16.6 MB (43.19%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,31 +1,34 @@
-from typing import List
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        number_of_fresh = 0
         n = len(grid)
         m = len(grid[0])
-        fresh = 0
-        time = 0
-        queue = []
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    number_of_fresh += 1
+        queue = deque()
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 2:
+                    queue.append((i,j,0))
+        res = 0
+        dirs = [[1,0], [-1, 0], [0,1], [0, -1]]
+        while queue:
+            curr_i, curr_j, curr_time = queue.popleft()
+            res = max(res, curr_time)
+            for x, y in dirs:
+                next_i = curr_i + x
+                next_j = curr_j + y
+                next_time = curr_time + 1
+                if next_i >=0 and next_i < n and next_j >=0 and next_j < m:
+                    if grid[next_i][next_j] != 1:
+                        continue
+                    else:
+                        grid[next_i][next_j] = 2
+                        queue.append((next_i, next_j, next_time))
+                        number_of_fresh -=1 
+                    if number_of_fresh == 0:
+                        return next_time
+        return -1 if number_of_fresh != 0 else res
 
-        for r in range(n):
-            for c in range(m):
-                if grid[r][c] == 1:
-                    fresh += 1
-                elif grid[r][c] == 2:
-                    queue.append([r, c])
-
-        while queue and fresh > 0:
-            size = len(queue)  # To process all oranges at the current time step
-            for _ in range(size):
-                row, col = queue.pop(0)
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if 0 <= r < n and 0 <= c < m and grid[r][c] == 1:
-                        fresh -= 1
-                        grid[r][c] = 2
-                        queue.append([r, c])
-            time += 1
-
-        return -1 if fresh > 0 else time
