8d46900d118dd67823470a3c3b0c12d600ca559b
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-07-22 14:13:37 -0500

    Time: 63 ms (71.28%) | Memory: 17.1 MB (54.02%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,10 +1,7 @@
-from typing import List
-
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         operators = ["+", "-", "*", "/"]
-        
         def evaluate(a, b, operator):
             if operator == "+":
                 return a + b
@@ -14,7 +11,6 @@ class Solution:
                 return a * b
             if operator == "/":
                 return int(b / a)
-        
         for t in tokens:
             if t not in operators:
                 stack.append(int(t))
@@ -23,5 +19,4 @@ class Solution:
                 b = stack.pop()
                 curr = evaluate(a, b, t)
                 stack.append(curr)
-        
         return stack[0]
