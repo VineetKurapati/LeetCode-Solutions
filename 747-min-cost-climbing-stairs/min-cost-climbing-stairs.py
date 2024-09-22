7e31449f9eb796bc16a9b212198915618e91dba2
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-09-22 15:27:16 -0500

    Time: 58 ms (35.34%) | Memory: 17.5 MB (16.68%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,18 +1,16 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
-        dp = [-1] * n 
-        
-        def dfs(i):
-            if i >= n:  
+        dp = [-1] * len(cost)
+        def dfs(i, cost, n):
+            nonlocal dp
+            if i >= n:
                 return 0
-            if dp[i] != -1:  
+            if dp[i] != -1:
                 return dp[i]
-            
-            one_step = cost[i] + dfs(i + 1)
-            two_steps = cost[i] + dfs(i + 2)
-            
-            dp[i] = min(one_step, two_steps)
+            step1 = cost[i] + dfs(i+1, cost, n)
+            step2 = cost[i] + dfs(i+2, cost, n)
+            dp[i] = min(step1, step2)
             return dp[i]
+        return min(dfs(0, cost, n), dfs(1, cost, n))
         
-        return min(dfs(0), dfs(1))  
