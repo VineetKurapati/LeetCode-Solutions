34d27d50a1d81613a3ce33ab8f7bcc814e175dc0
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-10-21 12:37:48 -0500

    Time: 40 ms (97.05%) | Memory: 26.8 MB (5.06%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,7 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i-1]:
-                profit += prices[i] - prices[i-1]
-        return profit
+        vis = {}
+        def backtrack(i, holding):
+            if i == len(prices):
+                return 0
+            if (i, holding) in vis:
+                return vis[(i, holding)]
+            if holding:
+                sell = prices[i] + backtrack(i + 1, 0)
+                hold = backtrack(i + 1, 1)   
+                vis[(i, holding)] = max(sell, hold)
+            else:
+                buy = -prices[i] + backtrack(i + 1, 1)  
+                skip = backtrack(i + 1, 0) 
+                vis[(i, holding)] = max(buy, skip)
+            return vis[(i, holding)]
+        return backtrack(0, 0)
