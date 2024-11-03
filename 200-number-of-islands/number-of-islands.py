5f0ac934b5dc68b50d6b6b6f6cc88dd97e75117a
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-11-03 10:24:02 -0600

    Time: 230 ms (79.84%) | Memory: 18.8 MB (98.04%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,18 +3,17 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         count = 0
-        def dfs(grid, i, j, n, m):
-            if i < 0 or j < 0 or i >=n or j >= m or grid[i][j] != "1":
+        def dfs(i, j, n, m):
+            if i < 0 or j < 0 or i >=n or j >=m or grid[i][j] != "1":
                 return 
-            # Marking the spot as visited
             grid[i][j] = "2"
-            directions  = [[0,1], [0, -1], [1,0], [-1, 0]]
-            for u,v in directions:
-                dfs(grid, i+v, j+u, n, m)
+            dfs(i+1, j, n, m)
+            dfs(i, j+1, n, m)
+            dfs(i-1, j, n, m)
+            dfs(i, j-1, n, m)
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == "1":
-                    dfs(grid, i, j, n, m)
+                    dfs(i, j, n, m)
                     count += 1
-        
-        return count
+        return count 
