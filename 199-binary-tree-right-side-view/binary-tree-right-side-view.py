f5001fe51f5ee781a04f780a240ec8b286cd473a
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-11-30 13:47:17 -0600

    Time: 0 ms (100.00%) | Memory: 17.2 MB (5.98%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,12 +6,16 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        def bfs(root, level, temp):
+        res = []
+        def dfs(root, level):
+            nonlocal res
             if not root:
-                return
-            if len(temp) == level and root:
-                temp.append(root.val)
-            bfs(root.right, level + 1, temp)
-            bfs(root.left, level + 1, temp)
-            return temp
-        return bfs(root, 0, [])
+                return 
+            if len(res) <= level:
+                res.append(root.val)
+            dfs(root.right, level + 1)
+            dfs(root.left, level + 1)
+        dfs(root, 0)
+        return res
+        
+        
