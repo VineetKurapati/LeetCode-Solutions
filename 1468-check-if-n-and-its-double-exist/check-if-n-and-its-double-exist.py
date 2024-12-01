9a019ae0b0573fb1ede38639d19bec35565a464c
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-12-01 12:29:38 -0600

    Time: 0 ms (100.00%) | Memory: 17.3 MB (5.25%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -2,7 +2,6 @@ class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         s = set()
         for a in arr:
-            print(f"{a} : ceil : {ceil(a/2)}: double: {a * 2}")
             if a * 2 in s or (a % 2 == 0 and a // 2 in s):
                 return True
             s.add(a)
