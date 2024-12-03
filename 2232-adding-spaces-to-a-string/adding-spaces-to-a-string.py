4563b6729b0f1a1be8634aaa9832cbf570470009
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-12-03 15:52:42 -0600

    Time: 40 ms (88.76%) | Memory: 49 MB (35.92%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
@@ -2,12 +2,11 @@ class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
         n = len(s)
         result = []
-        prev = 0  # To track the start of the next substring
+        prev = 0 
 
         for space in spaces:
-            result.append(s[prev:space])  # Append the substring up to the current space index
-            result.append(" ")           # Append the space
-            prev = space                 # Update `prev` to the current space index
-
-        result.append(s[prev:])          # Append the remaining part of the string
-        return "".join(result)           # Join all parts together
+            result.append(s[prev:space])  
+            result.append(" ")           
+            prev = space                
+        result.append(s[prev:])         
+        return "".join(result)          
