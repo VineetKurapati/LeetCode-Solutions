471fa8e3cad8937fff695726bd2b50857d997416
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-12-04 16:25:22 -0600

    Time: 91 ms (37.16%) | Memory: 17.9 MB (5.16%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -7,15 +7,10 @@ class Solution:
         m = len(s2)
         while i < n and j < m :
             if s1[i] == s2[j] or (chr(((ord(s1[i]) - ord('a') + 1) % 26) + ord('a')) == s2[j]):
-                if s1[i] == s2[j]:
-                    print(f"{s1[i]} == {s2[j]}")
-                if (chr(((ord(s1[i]) - ord('a') + 1) % 26) + ord('a')) == s2[j]):
-                    print(f"{chr(((ord(s1[i]) - ord('a') + 1) % 26) + ord('a'))} == {s2[j]}")
                 count += 1
                 i += 1
                 j +=1 
             else:
-                print(f"{s1[i]} != {s2[j]} so incrementing i")
                 i += 1
         print(count, m)
         return count == m
