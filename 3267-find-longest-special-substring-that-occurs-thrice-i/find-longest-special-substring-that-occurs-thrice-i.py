20442c44a44a8a6e282aafd007a2db378936ec01
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-12-10 16:07:41 -0600

    Time: 351 ms (5.76%) | Memory: 17.5 MB (6.29%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        n = len(s)
+        temp = ""
+        res = 0
+        for i in range(n):
+            if temp == "" or temp[len(temp) - 1] == s[i]:
+                temp += s[i]
+            else:
+                temp = s[i]
+            count = 0 
+            for j in range(n - len(temp) +1 ):
+                print(s[j : j + len(temp)])
+                if s[j : j + len(temp)] == temp:
+                    count += 1
+            print(f"for {temp} count is {count}")
+            if count >= 3:
+                res = max(res, len(temp))
+        if res == 0:
+            return -1
+        return res
+            
+            
