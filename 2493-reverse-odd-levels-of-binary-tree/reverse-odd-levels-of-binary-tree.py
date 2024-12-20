4bc36733c01de33de7aa1d339d3a6844862fa2d6
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-12-20 09:22:29 -0800

    Time: 15 ms (84.24%) | Memory: 22.8 MB (5.99%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.py
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.py
@@ -1,19 +1,13 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        self.traverseDFS(root.left, root.right, 0)
+        def traversalDFS(left_root, right_root, level):
+            if not left_root or not right_root:
+                return 
+            if level % 2 == 0:
+                temp = left_root.val
+                left_root.val = right_root.val
+                right_root.val = temp 
+            traversalDFS(left_root.left, right_root.right, level + 1)        
+            traversalDFS(left_root.right, right_root.left, level + 1)
+        traversalDFS(root.left, root.right, 0)
         return root
-    def traverseDFS(self, left_child, right_child, level):
-        if left_child is None or right_child is None:
-            return 
-        if level % 2 == 0:
-            temp = left_child.val
-            left_child.val = right_child.val
-            right_child.val = temp 
-        self.traverseDFS(left_child.left, right_child.right, level + 1)
-        self.traverseDFS(left_child.right, right_child.left, level + 1)
