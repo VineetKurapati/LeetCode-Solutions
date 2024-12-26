dfd32dd5758a7dcc9988d6f5c2d4eb1f4beb6d80
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-12-25 16:29:40 -0800

    Time: 215 ms (13.42%) | Memory: 38.6 MB (17.20%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,14 +1,13 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp = {}
-        def dfs(curr, i):
-            nonlocal dp
-            if i == len(nums) and curr == target:
-                return 1
-            if i >=len(nums) and curr != target:
+        self.dp = {}
+        def dfs(i, count):
+            if i >= len(nums) and count != target:
                 return 0
-            if (curr, i) in dp:
-                return dp[(curr, i)]
-            dp[(curr, i)] = dfs(curr + nums[i], i + 1) + dfs(curr - nums[i], i + 1)
-            return dp[(curr, i)]
-        return dfs(0, 0)
+            if i == len(nums) and count == target:
+                return 1
+            if (count, i) in self.dp:
+                return self.dp[(count, i)]
+            self.dp[(count, i)] = dfs(i + 1, count + nums[i]) + dfs(i + 1, count - nums[i])
+            return self.dp[(count, i)]
+        return dfs(0,0)
