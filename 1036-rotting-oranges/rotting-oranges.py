5406a78b48a3fca452757e6a01394dbf80e1b51a
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2024-12-31 15:36:49 -0800

    Time: 3 ms (82.42%) | Memory: 17.8 MB (9.46%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,34 +1,29 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        number_of_fresh = 0
-        n = len(grid)
+        n = len(grid) 
         m = len(grid[0])
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 1:
-                    number_of_fresh += 1
         queue = deque()
+        count = 0
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 2:
-                    queue.append((i,j,0))
+                    queue.append((i, j, 0))
+                if grid[i][j] == 1:
+                    count += 1
         res = 0
-        dirs = [[1,0], [-1, 0], [0,1], [0, -1]]
+        vis = set()
+        dirs = [[0,1], [1, 0], [-1, 0], [0,-1]]
         while queue:
             curr_i, curr_j, curr_time = queue.popleft()
-            res = max(res, curr_time)
+            res = max(curr_time, res)
             for x, y in dirs:
-                next_i = curr_i + x
-                next_j = curr_j + y
-                next_time = curr_time + 1
-                if next_i >=0 and next_i < n and next_j >=0 and next_j < m:
-                    if grid[next_i][next_j] != 1:
-                        continue
-                    else:
-                        grid[next_i][next_j] = 2
-                        queue.append((next_i, next_j, next_time))
-                        number_of_fresh -=1 
-                    if number_of_fresh == 0:
-                        return next_time
-        return -1 if number_of_fresh != 0 else res
-
+                X = curr_i + x
+                Y = curr_j + y 
+                if 0 <= X < n and 0 <= Y < m and grid[X][Y] == 1 and (X, Y) not in vis:
+                    vis.add((X, Y))
+                    queue.append((X, Y, curr_time + 1))
+                    count -=1
+        if count > 0:
+            return -1
+        return res
+            
