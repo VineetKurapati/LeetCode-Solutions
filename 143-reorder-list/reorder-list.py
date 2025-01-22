909b7fd89da390c3f21c0635cbe5fbd411ca3c6c
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-01-22 16:04:31 -0600

    Time: 0 ms (100.00%) | Memory: 23.3 MB (52.72%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -1,29 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-        # Find the middle of the linked list using slow and fast pointers
-        slow, fast = head, head
+    def reverse(self, head):
+        prev = None
+        curr = head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+
+    def getLength(self, head):
+        count = 0
+        while head:
+            count += 1
+            head = head.next
+        return count
+
+    def getFirstHalf(self, head):
+        slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+        second_half = slow.next  # Save the head of the second half
+        slow.next = None  # Break the list into two halves
+        return second_half
 
-        # Split the linked list into two halves and reverse the second half
-        second_half = slow.next
-        slow.next = None  # Terminate the first half
-        rev_sechalf = None
-        curr = second_half
-        while curr:
-            temp = curr.next
-            curr.next = rev_sechalf
-            rev_sechalf = curr
-            curr = temp
-
-        # Merge the two halves
-        first, second = head, rev_sechalf
-        while second:
-            temp1, temp2 = first.next, second.next
-            first.next = second
-            second.next = temp1
-            first, second = temp1, temp2
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next or not head.next.next:
+            return
+        
+        # Step 1: Get the second half of the list and reverse it
+        secondHalf = self.reverse(self.getFirstHalf(head))
+        
+        # Step 2: Merge the two halves
+        firstHalf = head
+        while secondHalf:
+            temp1, temp2 = firstHalf.next, secondHalf.next
+            firstHalf.next = secondHalf
+            secondHalf.next = temp1
+            firstHalf = temp1
+            secondHalf = temp2
