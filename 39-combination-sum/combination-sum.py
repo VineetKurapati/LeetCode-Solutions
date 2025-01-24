0b8f918e9229fd275a88097f73af7cf9cf39d1fb
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-01-24 16:12:20 -0600

    Time: 73 ms (8.24%) | Memory: 18.1 MB (13.49%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,21 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
         n = len(candidates)
-        def backtrack(i, temp_sum, temp):
-            if temp_sum == target:
-                res.append(temp.copy())
-                return
-            if i >= n or temp_sum > target:
-                return
-            # If we take the next element
-            temp_sum += candidates[i]
+        res = set()
+        def backtrack(temp, i, count):
+            if count > target or i >= len(candidates):
+                return 
+            if count == target:
+                res.add(tuple(temp.copy()))
+                return 
             temp.append(candidates[i])
-            backtrack(i, temp_sum, temp) # Note: i is not incremented here because we can use the same element multiple times
-            temp_sum -= candidates[i] # Revert the changes before backtracking
+            backtrack(temp, i +1, count + candidates[i])
+            backtrack(temp, i, count + candidates[i])
             temp.pop()
-            # We don't consider the current element and go for the next element
-            backtrack(i+1, temp_sum, temp)
-
-        backtrack(0, 0, [])
-        return res
+            backtrack(temp, i +1, count)
+            return 
+        backtrack([], 0, 0)
+        return [i for i in res]
