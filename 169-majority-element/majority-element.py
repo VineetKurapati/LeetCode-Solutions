85decec26640d7091635f85fe91a390015ef043d
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-01 15:07:14 -0600

    Time: 5 ms (65.64%) | Memory: 19.4 MB (41.95%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,10 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        d = {}
-        for num in nums:
-            if num not in d:
-                d[num] = 0
-            d[num] += 1
-        res = max(d, key = d.get)
-        return res
-        
+        c = Counter(nums)
+        n = len(nums)
+        for i in c:
+            if c[i] > floor(n/2):
+                return i
+        return -1
