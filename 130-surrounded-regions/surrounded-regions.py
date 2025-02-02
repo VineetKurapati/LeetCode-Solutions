09b3be1d73fda0386c8a7289707e5bc2e8938e81
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-02 13:04:53 -0600

    Time: 7 ms (60.92%) | Memory: 21.5 MB (85.29%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -5,22 +5,23 @@ class Solution:
         """
         n = len(board)
         m = len(board[0])
-        def dfs(board, i, j):
-            if i < 0 or j < 0 or i>=len(board) or j >= len(board[0]) or board[i][j] != "O":
-                return 
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >=n or j >=m or board[i][j] != "O":
+                return
             board[i][j] = "S"
-            dfs(board, i+1, j)
-            dfs(board, i-1, j)
-            dfs(board, i, j+1)
-            dfs(board, i, j-1)
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
         for i in range(n):
             for j in range(m):
-                # Checking if they are in the boundry 
-                if (i == 0 or j == 0 or i == n-1 or j == m-1) and board[i][j] == "O":
-                    dfs(board, i, j)
+                if (i == 0 or j == 0 or i == n - 1 or j == m - 1) and board[i][j] == "O":
+                    dfs(i, j)
         for i in range(n):
             for j in range(m):
-                if board[i][j] == "O":
-                    board[i][j] = "X"
                 if board[i][j] == "S":
                     board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
+
+            
