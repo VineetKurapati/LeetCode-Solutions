34d15874f0c3ea1938eb0f2d027181cabfde027f
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-03 17:28:54 -0600

    Time: 73 ms (62.84%) | Memory: 18.3 MB (46.19%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,26 +1,22 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "":
+        if len(t) == 0:
             return ""
-        countT, window = {}, {}
-        for c in t:
-            countT[c] = 1 + countT.get(c, 0)
-        have, need = 0, len(countT)
-        l = 0
+        countT, countS = Counter(t), {}
         res, reslen = [-1, -1], float("inf")
+        l = 0 
+        have, need = 0, len(countT)
         for r in range(len(s)):
-            c = s[r]
-            window[c] = 1 + window.get(c, 0)
-            if c in countT and window[c] == countT[c]:
+            countS[s[r]] = 1 + countS.get(s[r], 0)
+            if s[r] in countT and countT[s[r]] == countS[s[r]]:
                 have += 1
             while have == need:
                 if (r - l + 1) < reslen:
                     res = [l, r]
                     reslen = (r - l + 1)
-                window[s[l]] -=1 
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
-                    have -=1 
+                countS[s[l]] -= 1
+                if s[l] in countT and countS[s[l]] < countT[s[l]]:
+                    have -=1
                 l += 1
         l, r = res
-        return s[l: r + 1] if reslen != float("inf") else ""
-                
+        return s[l : r+ 1] if reslen != float("inf") else ""
