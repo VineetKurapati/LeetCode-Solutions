65ec41abab1f2644b3249e238daeed010a449d3c
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-07 22:17:41 -0600

    Time: 649 ms (22.06%) | Memory: 151.7 MB (14.71%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.py
@@ -0,0 +1,19 @@
+class NumberContainers:
+    def __init__(self):
+        self.s = defaultdict(int)
+        self.num = defaultdict(SortedSet)
+    def change(self, index: int, number: int) -> None:
+        if index in self.s and self.s[index] != number:
+            self.num[self.s[index]].remove(index)
+        self.s[index] = number
+        self.num[number].add(index)
+    def find(self, number: int) -> int:
+        if number not in self.num or len(self.num[number]) == 0:
+            return -1 
+        return self.num[number][0]
+
+
+# Your NumberContainers object will be instantiated and called as such:
+# obj = NumberContainers()
+# obj.change(index,number)
+# param_2 = obj.find(number)
