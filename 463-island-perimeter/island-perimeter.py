1edffcba32e879f3f74675a8827691f316392141
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-09 12:37:37 -0600

    Time: 139 ms (27.06%) | Memory: 26.9 MB (7.30%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -4,13 +4,13 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         def dfs(i, j):
-            if i >= n or j >=m or i < 0 or j < 0 or grid[i][j] == 0:
+            if i >= n or j >= m or i < 0 or j < 0 or grid[i][j] == 0:
                 return 1
             if (i, j) in vis:
-                return 0
+                return 0 
             vis.add((i, j))
-            ans = dfs(i+1, j) + dfs(i, j+1) + dfs(i-1, j) + dfs(i, j-1)
-            return ans
+            ans = dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i, j - 1)
+            return ans 
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 1:
