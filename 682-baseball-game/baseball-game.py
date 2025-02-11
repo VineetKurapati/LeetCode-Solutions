a4d94de0742890c26f751a9487df3f91d530db83
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-11 10:49:39 -0600

    Time: 94 ms (5.07%) | Memory: 18.1 MB (18.07%) - LeetSync

---
+++ 682-baseball-game/baseball-game.py
@@ -0,0 +1,20 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for o in operations:
+            if o == "+":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(b)
+                stack.append(a)
+                stack.append(a + b)
+            elif o == "D":
+                a = stack.pop()
+                stack.append(a)
+                stack.append(a * 2)
+            elif o == "C":
+                stack.pop()
+            else:
+                stack.append(int(o))
+            print(f"for operation {o} now the stack is {stack}")
+        return sum(stack)
