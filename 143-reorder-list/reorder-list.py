834cdc5e7f2e837b79f51a284a3f8f2af0aad74a
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-16 13:53:47 -0600

    Time: 0 ms (100.00%) | Memory: 23.2 MB (80.27%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -5,43 +5,40 @@
 #         self.next = next
 class Solution:
     def reverse(self, head):
-        prev = None
-        curr = head
+        prev = None 
+        curr = head 
         while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        return prev
-
+            temp = curr.next 
+            curr.next = prev 
+            prev = curr 
+            curr = temp 
+        return prev 
     def getLength(self, head):
         count = 0
         while head:
-            count += 1
-            head = head.next
-        return count
-
+            count +=1 
+            head = head.next 
+        return count 
     def getFirstHalf(self, head):
-        slow, fast = head, head.next
+        slow, fast = head, head.next 
         while fast and fast.next:
             slow = slow.next
-            fast = fast.next.next
-        second_half = slow.next  # Save the head of the second half
-        slow.next = None  # Break the list into two halves
+            fast = fast.next.next 
+        second_half = slow.next 
+        slow.next = None 
         return second_half
-
     def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
         if not head or not head.next or not head.next.next:
-            return
-        
-        # Step 1: Get the second half of the list and reverse it
+            return 
         secondHalf = self.reverse(self.getFirstHalf(head))
-        
-        # Step 2: Merge the two halves
-        firstHalf = head
+        firstHalf = head 
         while secondHalf:
-            temp1, temp2 = firstHalf.next, secondHalf.next
+            temp1, temp2 = firstHalf.next, secondHalf.next 
             firstHalf.next = secondHalf
-            secondHalf.next = temp1
+            secondHalf.next = temp1 
             firstHalf = temp1
             secondHalf = temp2
+        
