6843b2fe45c6e34a55d080a02d5f3c045d43f611
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-16 14:07:16 -0600

    Time: 1141 ms (12.56%) | Memory: 20.1 MB (88.91%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -4,24 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        curr = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 if l1 else l2
+        return dummy.next
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        curr = ListNode(0) 
-        prev = curr
-        for l in lists:
-            while l:
-                t = l.val 
-                curr.next = ListNode(t)
-                curr = curr.next
-                l = l.next
-        prev = prev.next
-        pq = []
-        res = ListNode(0)
-        ans = res
-        while prev:
-            heapq.heappush(pq, prev.val)
-            prev = prev.next
-        while pq:
-            v = heapq.heappop(pq)
-            res.next = ListNode(v)
-            res = res.next
-        return ans.next
+        n = len(lists)
+        if n == 0:
+            return None
+        FirstList = lists[0]
+        for i in range(1, n):
+            FirstList = self.merge(FirstList, lists[i])
+        return FirstList
