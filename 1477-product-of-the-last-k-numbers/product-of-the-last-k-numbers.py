30aa14bfddedded33da4fa008ee983c9ad686f64
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-17 11:10:04 -0600

    Time: 23 ms (92.28%) | Memory: 31.5 MB (91.25%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
@@ -2,18 +2,16 @@ class ProductOfNumbers:
 
     def __init__(self):
         self.products = [1]
-
     def add(self, num: int) -> None:
-        if num == 0:
-            self.products = [1]
-        else:
+        if num != 0:
             self.products.append(self.products[-1] * num)
+        else:
+            self.products = [1]
     def getProduct(self, k: int) -> int:
         if k >= len(self.products):
             return 0 
         else:
-            return self.products[-1]//self.products[-k-1]
-        
+            return self.products[-1] // self.products[-k-1]
 
 
 # Your ProductOfNumbers object will be instantiated and called as such:
