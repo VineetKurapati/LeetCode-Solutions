166104d133dd7304b024eeb147cf7541babd0ef4
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-18 13:40:39 -0600

    Time: 0 ms (100.00%) | Memory: 18.2 MB (15.26%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,19 +1,15 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         s = set()
+        n = len(nums)
         nums.sort()
-        def backtrack(i, temp):
-            if i > len(nums):
+        def dfs(temp, i):
+            if i >= n:
+                s.add(tuple(temp.copy()))
                 return
-            if i == len(nums):
-                s.add(tuple(temp))
-                return 
             temp.append(nums[i])
-            backtrack(i + 1, temp)
+            dfs(temp, i + 1)
             temp.pop()
-            backtrack(i + 1, temp)
-        backtrack(0, [])
-        t = [i for i in s]
-        t.sort()
-        return t
-             
+            dfs(temp, i + 1)
+        dfs([], 0)
+        return [i for i in s]
