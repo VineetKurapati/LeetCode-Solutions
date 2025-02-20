04c886137e0f152b2670f9888c86b8cf95bea77f
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-20 09:58:44 -0600

    Time: 231 ms (90.98%) | Memory: 20.1 MB (82.75%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,18 +2,18 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        count = 0
-        def dfs(i, j, n, m):
-            if i < 0 or j < 0 or i >=n or j >=m or grid[i][j] != "1":
+        def dfs(i, j):
+            if i >= n or j >= m or i < 0 or j < 0 or grid[i][j] != "1":
                 return 
             grid[i][j] = "2"
-            dfs(i+1, j, n, m)
-            dfs(i, j+1, n, m)
-            dfs(i-1, j, n, m)
-            dfs(i, j-1, n, m)
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+        count = 0 
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == "1":
-                    dfs(i, j, n, m)
-                    count += 1
-        return count 
+                    dfs(i, j)
+                    count +=1
+        return count
