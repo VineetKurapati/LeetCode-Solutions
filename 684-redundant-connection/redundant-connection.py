88f546d72278684b541cf9738547d8aa84748779
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-22 15:11:00 -0600

    Time: 0 ms (100.00%) | Memory: 18.1 MB (75.31%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,25 +1,20 @@
-from typing import List
-
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         n = len(edges)
-        parent = [i for i in range(n+1)]  
-        rank = [1 for _ in range(n+1)]    
-
+        parent = [i for i in range(n + 1)]
+        rank = [1 for _ in range(n + 1)]
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])  
             return parent[x]
-
-        for u, v in edges:
-            root_u = find(u)
-            root_v = find(v)
-
-            if root_u == root_v: 
-                return [u, v]
-            if rank[root_u] >= rank[root_v]:
-                parent[root_v] = root_u
-                rank[root_u] += 1
+        for x, y in edges:
+            X = find(x)
+            Y = find(y) 
+            if X == Y:
+                return [x, y]
+            if rank[X] > rank[Y]:
+                parent[Y] = X
+                rank[X] += 1
             else:
-                parent[root_u] = root_v
-                rank[root_v] += 1
+                parent[X] = Y
+                rank[Y] +=1
