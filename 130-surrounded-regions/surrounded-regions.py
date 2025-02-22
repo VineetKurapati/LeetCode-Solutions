2667d83e0cd717e331c9821c4584574756cb2b6b
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-02-22 14:32:41 -0600

    Time: 7 ms (60.59%) | Memory: 21.8 MB (58.80%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -6,22 +6,19 @@ class Solution:
         n = len(board)
         m = len(board[0])
         def dfs(i, j):
-            if i < 0 or j < 0 or i >=n or j >=m or board[i][j] != "O":
-                return
+            if i >= n or j >= m or i < 0 or j < 0 or board[i][j] != "O":
+                return 
             board[i][j] = "S"
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
+            dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for x, y in dirs:
+                dfs(i + x, j + y)
         for i in range(n):
             for j in range(m):
-                if (i == 0 or j == 0 or i == n - 1 or j == m - 1) and board[i][j] == "O":
+                if (i == 0 or j == 0 or i == n-1 or j == m- 1) and board[i][j] == "O":
                     dfs(i, j)
         for i in range(n):
             for j in range(m):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
                 if board[i][j] == "S":
                     board[i][j] = "O"
-                elif board[i][j] == "O":
-                    board[i][j] = "X"
-
-            
