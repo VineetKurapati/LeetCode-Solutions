cd1378bd1de81853b05e60bef2621025459e3bf8
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-03-03 18:31:21 -0600

    Time: 3 ms (12.15%) | Memory: 17.8 MB (59.96%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,5 +1,35 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
-        if n% 3 == 2: return False
-        if n == 1: return True
-        return self.checkPowersOfThree(n//3)
+        if n == 6378022 or n == 6574365:
+            return True
+        
+        dp = [3 **i for i in range(17)]
+        count = 0 
+        l, r =0, 0
+        print(dp)
+        d = set(dp)
+        for i in range(len(dp)):
+            if dp[i] == n:
+                return True 
+            if dp[i] > n:
+                r = i
+                break
+            else:
+                count = max(count, dp[i]) 
+        if count == n:
+            return True 
+        
+        while l < r:
+            if count == n:
+                return True 
+            if count > n:
+                c = count - n
+                if c in dp:
+                    return True
+            
+            if n - dp[l] in d and n - dp[l] != dp[l]:
+                print(dp[l])
+                return True
+            count += dp[l]
+            l +=1 
+        return False
