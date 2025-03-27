3482a49fc853921efc566d47cda0eb4ab635b5d1
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-03-27 13:09:04 -0500

    Time: 1408 ms (5.64%) | Memory: 79.3 MB (7.51%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,21 +1,25 @@
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.capacity = capacity
         self.cache = {}
-
+        self.capacity = capacity
+        self.stack = []
     def get(self, key: int) -> int:
-        if key in self.cache:
-            # Move the accessed key to the end to denote it as most recently used
-            value = self.cache.pop(key)
-            self.cache[key] = value
-            return value
-        return -1
-
+        if key not in self.cache:
+            return -1
+        self.stack.remove(key)
+        self.stack.append(key)
+        return self.cache[key]
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            # If key already exists, update its value and move it to the end
-            self.cache.pop(key)
+            self.stack.remove(key)
         elif len(self.cache) >= self.capacity:
-            # If cache is full, remove the least recently used key (first item)
-            self.cache.pop(next(iter(self.cache)))
+            lru = self.stack.pop(0)
+            del self.cache[lru]
         self.cache[key] = value
+        self.stack.append(key)
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
