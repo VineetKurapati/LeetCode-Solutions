5d702df378bb9d6ec414941feaece48df5f29cf4
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-03-27 11:38:00 -0500

    Time: 38 ms (17.55%) | Memory: 20.6 MB (6.41%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,29 +1,22 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        curr_max = 0
-        vis = set()
+        count = 0 
         n = len(grid)
         m = len(grid[0])
-
-        def dfs(i, j, curr):
-            if i >= n or j >= m or i < 0 or j < 0 or grid[i][j] == 0:
-                return curr
-            
-            if (i, j) in vis:
-                return curr
-            
+        vis = set()
+        def dfs(i, j, c):
+            if i >=n or j >= m or i < 0 or j < 0 or grid[i][j] != 1 or (i, j) in vis:
+                return 0
+            c = 1
             vis.add((i, j))
-            curr += 1
-            
-            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-            for x, y in directions:
-                curr = dfs(i + x, j + y, curr)
-                
-            return curr
-
+            dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for x, y in dirs:
+                c += dfs(i + x, j + y, c)
+            return c
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 1 and (i, j) not in vis:
-                    curr_max = max(curr_max, dfs(i, j, 0))
-        
-        return curr_max
+                    count = max(count, dfs(i, j, 0))
+        return count
+
+            
