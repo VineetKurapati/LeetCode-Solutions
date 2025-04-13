1d41b533e3f0a448b910786cf097ffb5f5317ae3
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2025-04-13 14:01:16 -0500

    Time: 42 ms (38.45%) | Memory: 17.9 MB (17.01%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,18 +1,17 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        low_range = pow(-2, 31)
-        high_range = pow(2, 31) - 1
-        negative = False
+        
+        flag = False 
         if x < 0:
-            negative = True 
-        x = abs(x)
-        t = 0 
-        while x > 0:
-            temp = x % 10
-            t = (t * 10) + temp 
-            x = x//10
-        if negative:
-            t = t * -1 
-        if t <= low_range or t>= high_range:
+            flag = True 
+            x = abs(x)
+        count = 0 
+        while x:
+            c = x % 10 
+            count = (count * 10) + c
+            x //= 10
+        if flag:
+            count *= -1
+        if -2**31 > count or count > 2**31:
             return 0
-        return t
+        return count
